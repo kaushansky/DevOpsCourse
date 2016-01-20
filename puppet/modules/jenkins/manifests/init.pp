9b0e0f91d99317ec6a468b42ceabf26859a371c2
kaushansky <alex.kaushansky@gmail.com>
2016-01-20 15:30:02 +0200

    Update init.pp

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -1,35 +1,26 @@
 class jenkins {
+	require java
 	package { 'jenkins':
-		ensure  => present,
-		require => Yumrepo['jenkins'],
-		subscribe => Package['java-1.7.0-openjdk'],
+	  ensure  => present,
+	  require => Yumrepo['jenkins'],
 	}
-
 	yumrepo { 'jenkins':
-		baseurl  => 'http://pkg.jenkins-ci.org/redhat',
-		descr    => 'Jenkins',
-		enabled  => 1,
-		gpgcheck => 1,
-		gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key',
-		require  => File['/etc/pki/rpm-gpg/jenkins-ci.org.key'],
+	  baseurl  => 'http://pkg.jenkins-ci.org/redhat',
+	  descr    => 'Jenkins',
+	  enabled  => 1,
+	  gpgcheck => 1,
+	  gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key',
+	  require  => Wget['/etc/pki/rpm-gpg/jenkins-ci.org.key'],
 	}
-	  
-	file { '/etc/pki/rpm-gpg/jenkins-ci.org.key':
-		owner  => root,
-		group  => root,
-		mode   => 0600,
-		source => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+	wget { '/etc/pki/rpm-gpg/jenkins-ci.org.key':
+	  ensure   => present,
+	  url      => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
 	}
-	  service { 'jenkins':
-        ensure     => running,
-        hasstatus  => true,
-        hasrestart => true,
-        enable     => true,
-		subscribe  => Package['jenkins'],
-      }
-    
-    # install java
-      package { 'java-1.7.0-openjdk':
-        ensure => present,
+	service { 'jenkins':
+          ensure     => running,
+          hasstatus  => true,
+          hasrestart => true,
+          enable     => true,
+	  subscribe  => Package['jenkins'],
       }
 }
