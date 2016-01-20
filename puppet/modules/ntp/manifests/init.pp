89bc053737d21c15f281c1a8b1c9f06350507087
kaushansky <alex.kaushansky@gmail.com>
2016-01-20 13:26:19 +0200

    Update init.pp in ntp module

--- puppet/modules/ntp/manifests/init.pp
+++ puppet/modules/ntp/manifests/init.pp
@@ -1,1 +1,11 @@
-
+class ntp {
+  package { 'ntp':
+    ensure => installed,
+  }
+  service { 'ntpd':
+    ensure => running,
+    enable => true,
+    require => Package['ntp'],
+    restart => true,
+  }
+}
