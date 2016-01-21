c992924bc03767173fb690df73269d65c1fafe69
kaushansky <alex.kaushansky@gmail.com>
2016-01-21 15:30:03 +0200

    Update init.pp

--- puppet/modules/python/manifests/init.pp
+++ puppet/modules/python/manifests/init.pp
@@ -1,5 +1,19 @@
-class python{
-  package { [ 'python', 'python-dev', 'python-pip']:
-    ensure => present,
+class python {
+  package {'scl-utils':
+    ensure =>  present,
+    before => Package['centos-release-scl-rh'],
+  }
+  package {'centos-release-scl-rh':
+    ensure =>  present,
+    before => Package['python27'],
+  }
+  package { 'python27':
+    ensure =>  present,
+    before => Exec['enable_python27'],
+  }
+  exec {'enable_python27':
+    command => 'scl enable python27 bash',
+    provider => shell,
   }
 }
+
