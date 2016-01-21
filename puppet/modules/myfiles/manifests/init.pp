c0ba158ef7fac64a96967a56448923ef4f219810
kaushansky <alex.kaushansky@gmail.com>
2016-01-21 15:28:40 +0200

    Update init.pp

--- puppet/modules/myfiles/manifests/init.pp
+++ puppet/modules/myfiles/manifests/init.pp
@@ -1,24 +1,30 @@
 class myfiles {
-  file {['/etc/puppet/modules', 
+  file {['/etc/puppet/modules',
          '/etc/puppet/manifests',
-		 '/etc/puppet/modules/ntp',
-		 '/etc/puppet/modules/ntp/manifests',
-		 '/etc/puppet/modules/apache',
-		 '/etc/puppet/modules/apache/manifests',
-		 '/etc/puppet/modules/git',
+         '/etc/puppet/modules/ntp',
+         '/etc/puppet/modules/ntp/manifests',
+         '/etc/puppet/modules/apache',
+         '/etc/puppet/modules/apache/manifests',
+         '/etc/puppet/modules/git',
          '/etc/puppet/modules/git/manifests',
-		 '/etc/puppet/modules/jenkins',
+         '/etc/puppet/modules/jenkins',
          '/etc/puppet/modules/jenkins/manifests',
-		 '/etc/puppet/modules/maven',
+         '/etc/puppet/modules/maven',
          '/etc/puppet/modules/maven/manifests',
-		 '/etc/puppet/modules/python',
+         '/etc/puppet/modules/python',
          '/etc/puppet/modules/python/manifests',
          '/etc/puppet/modules/java',
          '/etc/puppet/modules/java/manifests']:
      ensure => 'directory',
-	 before => [Wget['/etc/puppet/modules/python/manifests/init.pp'],Wget['/etc/puppet/modules/maven/manifests/init.pp'],Wget['/etc/puppet/modules/jenkins/manifests/init.pp'],Wget['/etc/puppet/modules/ntp/manifests/init.pp'],Wget['/etc/puppet/modules/apache/manifests/init.pp'],Wget['/etc/puppet/modules/git/manifests/init.pp']]
-  }
-      
+     before => [Wget['/etc/puppet/modules/java/manifests/init.pp'],
+                Wget['/etc/puppet/modules/python/manifests/init.pp'],
+                Wget['/etc/puppet/modules/maven/manifests/init.pp'],
+                Wget['/etc/puppet/modules/jenkins/manifests/init.pp'],
+                Wget['/etc/puppet/modules/ntp/manifests/init.pp'],
+                Wget['/etc/puppet/modules/apache/manifests/init.pp'],
+                Wget['/etc/puppet/modules/git/manifests/init.pp']]
+}
+
   wget {'/etc/puppet/modules/ntp/manifests/init.pp':
     ensure   => present,
     url    => 'https://raw.githubusercontent.com/kaushansky/DevOpsCourse/master/puppet/modules/ntp/manifests/init.pp',
@@ -48,4 +54,3 @@ class myfiles {
     url    => 'https://raw.githubusercontent.com/kaushansky/DevOpsCourse/master/puppet/modules/java/manifests/init.pp',
   }
 }
-
