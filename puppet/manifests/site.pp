c09479630e94ffcfa82347dcef4c855b5cbac6fb
kaushansky <alex.kaushansky@gmail.com>
2016-02-02 20:28:38 +0200

    Update site.pp

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,28 +2,39 @@ node default {
 
 }
 
-node 'puppet.amat.com' {
+node "ak-control.ak-control.g10.internal.cloudapp.net" {
   Class['myfiles'] -> Class['ntp']
   include myfiles
   include ntp
 }
-node 'build-node-01.amat.com' {
+node "build-node-01.build-node-01.g3.internal.cloudapp.net" {
   Class['ntp'] -> Class['java'] -> Class['maven'] -> Class['git']
   include ntp
   include java
   include maven
   include git
 }
-node 'jenkins.amat.com' {
+node "ak-jenkins.ak-jenkins.g6.internal.cloudapp.net" {
   Class['ntp'] -> Class['java'] -> Class['jenkins']
   include ntp
   include java
   include jenkins
 }
-node 'webserver.amat.com' {
-  Class['ntp'] -> Class['apache'] -> Class['python']
+node "ak-webserver.ak-webserver.g10.internal.cloudapp.net" {
+  Class['ntp'] -> Class['python']
   include ntp
-  include apache
   include python
+  class { 'epel': }->
+          tomcat::instance{ 'default':
+          package_name => 'tomcat',
+        }->
+        tomcat::service { 'default':
+          use_jsvc     => false,
+          use_init     => true,
+          service_name => 'tomcat',
+        }
+  wget {'/etc/jenkins/getwar.py':
+    ensure   => present,
+    url    => 'https://raw.githubusercontent.com/kaushansky/DevOpsCourse/master/webserver/getwar.py',
+  }
 }
-
