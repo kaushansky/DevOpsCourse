7627abaddb0a7469a75d7114bd901910d65a2d99
kaushansky <alex.kaushansky@gmail.com>
2016-02-02 20:32:07 +0200

    Update init.pp

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -1,26 +1,26 @@
 class jenkins {
-	require java
-	package { 'jenkins':
-	  ensure  => present,
-	  require => Yumrepo['jenkins'],
-	}
-	yumrepo { 'jenkins':
-	  baseurl  => 'http://pkg.jenkins-ci.org/redhat',
-	  descr    => 'Jenkins',
-	  enabled  => 1,
-	  gpgcheck => 1,
-	  gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key',
-	  require  => Wget['/etc/pki/rpm-gpg/jenkins-ci.org.key'],
-	}
-	wget { '/etc/pki/rpm-gpg/jenkins-ci.org.key':
-	  ensure   => present,
-	  url      => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
-	}
-	service { 'jenkins':
+        require java
+        package { 'jenkins':
+          ensure  => present,
+          require => Yumrepo['jenkins'],
+        }
+        yumrepo { 'jenkins':
+          baseurl  => 'http://pkg.jenkins-ci.org/redhat',
+          descr    => 'Jenkins',
+          enabled  => 1,
+          gpgcheck => 1,
+          gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key',
+          require  => Wget['/etc/pki/rpm-gpg/jenkins-ci.org.key'],
+        }
+        wget { '/etc/pki/rpm-gpg/jenkins-ci.org.key':
+          ensure   => present,
+          url      => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+        }
+        service { 'jenkins':
           ensure     => running,
           hasstatus  => true,
           hasrestart => true,
           enable     => true,
-	  subscribe  => Package['jenkins'],
+          subscribe  => Package['jenkins'],
       }
 }
